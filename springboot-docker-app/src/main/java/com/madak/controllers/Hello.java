69e75316972f0890dfbc4348934b8e470e14ec20
Er.Niranjan Madak <nmadak28@gmail.com>
2021-01-20 06:43:49 +0530

    commit done

---
+++ springboot-docker-app/src/main/java/com/madak/controllers/Hello.java
@@ -0,0 +1,16 @@
+package com.madak.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rest/docker/hello")
+public class Hello {
+	
+	@GetMapping
+	public String hello() {
+		return "hello niranjan welcome to first docker application !";
+	}
+
+}
